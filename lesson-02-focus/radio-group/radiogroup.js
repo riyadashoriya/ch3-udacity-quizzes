ed6e5a2657887327db534da5103b40e5597367b8
Riya Dashoriya <riyadashoriya@gmail.com>
2018-05-25 09:37:00 -0700

    D27 part 3 - lesson 2 - ch 14 Roving focus.
    select value from checkbox by using tabindex value and tabs.

---
+++ lesson-02-focus/radio-group/radiogroup.js
@@ -0,0 +1,73 @@
+(function() {
+  'use strict';
+
+  // Define values for keycodes
+  var VK_ENTER      = 13;
+  var VK_SPACE      = 32;
+  var VK_LEFT       = 37;
+  var VK_UP         = 38;
+  var VK_RIGHT      = 39;
+  var VK_DOWN       = 40;
+
+  // Helper function to convert NodeLists to Arrays
+  function slice(nodes) {
+    return Array.prototype.slice.call(nodes);
+  }
+
+  function RadioGroup(id) {
+    this.el = document.querySelector(id);
+    this.buttons = slice(this.el.querySelectorAll('.radio'));
+    this.focusedIdx = 0;
+    this.focusedButton = this.buttons[this.focusedIdx];
+
+    this.el.addEventListener('keydown', this.handleKeyDown.bind(this));
+  }
+
+  RadioGroup.prototype.handleKeyDown = function(e) {
+    switch(e.keyCode) {
+
+      case VK_UP:
+      case VK_LEFT: {
+
+        e.preventDefault();
+
+        // This seems like a good place to do some stuff :)
+        //If you are pressing left or up key, check if the position is 0, if yes, then move the
+        //focus to the last element. Else, go one up.
+        this.focusedIdx = this.focusedIdx === 0 ? this.buttons.length - 1 : this.focusedIdx - 1;
+        break;
+
+      }
+
+      case VK_DOWN:
+      case VK_RIGHT: {
+
+        e.preventDefault();
+
+        // This seems like a good place to do some stuff :)
+        //If you are pressing right or down key, check if the focus is on the last element,
+        //if yes, then the focus should go on top, else move one down.
+        this.focusedIdx = this.focusedIdx === this.buttons.length - 1 ? 0 : this.focusedIdx + 1;
+        break;
+      }
+
+    }
+
+    this.changeFocus(this.focusedIdx); // <-- Hmm, interesting...
+  };
+
+  RadioGroup.prototype.changeFocus = function(idx) {
+    // Set the old button to tabindex -1
+    this.focusedButton.tabIndex = -1;
+    this.focusedButton.removeAttribute('checked');
+
+    // Set the new button to tabindex 0 and focus it
+    this.focusedButton = this.buttons[idx];
+    this.focusedButton.tabIndex = 0;
+    this.focusedButton.focus();
+    this.focusedButton.setAttribute('checked', 'checked');
+  };
+
+  var group1 = new RadioGroup('#group1');
+
+}());
